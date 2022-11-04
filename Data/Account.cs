d9978ea01f265f8e2322e999880cd5260cfe5087
EmanuelPatricio <emanuelptr81@gmail.com>
2022-11-04 10:36:32 -0400

    first commit

---
+++ Data/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAutoComplete.Data
+{
+  public class Account
+  {
+    public string AccountId { get; set; }
+    public string AccountAlias { get; set; }
+    public string IdWithAlias => $"{this.AccountId}/{this.AccountAlias}";
+  }
+}
---
+++ Services/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using BlazorAutoComplete.Data;
+using System.Text.RegularExpressions;
+
+namespace BlazorAutoComplete.Services
+{
+  public class AccountService
+  {
+    private static Random random = new Random();
+    public List<Account> GetAccounts()
+    {
+      var account = new List<Account>();
+
+      for (int i = 0; i < 10; i++)
+      {
+        account.Add(new Account()
+        {
+          AccountId = Regex.Replace(new string(Enumerable.Repeat("0123456789", 15)
+        .Select(s => s[random.Next(s.Length)]).ToArray()), @"^(...)(...)(...)(...)(...)$", "$1-$2-$3-$4-$5"),
+          AccountAlias = new string(Enumerable.Repeat("0123456789", 6)
+        .Select(s => s[random.Next(s.Length)]).ToArray())
+        });
+      }
+
+      return account;
+    }
+  }
+}
